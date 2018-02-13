8e118f0fe875074d712a41417ac8248699a4a229
fowafolo <anzhehong@126.com>
2018-02-13 16:00:05 -0500

    init iOS client

---
+++ iOS/Karaoke/Karaoke/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Karaoke
+//
+//  Created by 安哲宏 on 2/13/18.
+//  Copyright © 2018 cmu.edu. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
